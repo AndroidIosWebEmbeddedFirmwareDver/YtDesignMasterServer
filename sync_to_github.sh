1bb0958e98762e6c4599eeceebea9bae59002e54
wangxiaolong <494951601@qq.com>
2018-04-04 09:12:47 +0800

    first commit

---
+++ sync_to_github.sh
@@ -0,0 +1,10 @@
+#1.
+git init
+#2.
+git add ./
+#3.
+git commit -m "first commit"
+#4.
+git remote add origin https://github.com/AndroidIosWebEmbeddedFirmwareDver/YtDesignMasterServer.git
+#5.
+git push -u origin master
