05fdf73875c59321eb44ef0b6d4865dd53043909
wangxiaolong <494951601@qq.com>
2018-04-04 09:20:29 +0800

    $nowGMT

--- sync_to_github.sh
+++ sync_to_github.sh
@@ -1,9 +1,13 @@
+
+#In github's jekyll,you should enter GMT time (time zone UTC(+0:00))
+nowGMT='date -u +"%Y-%m-%d %H:%M:%S"'
+var='date "+%Y-%m-%d %H:%M:%S"'
 #1.
 git init
 #2.
 git add ./
 #3.
-git commit -m "first commit"
+git commit -m '$nowGMT'
 #4.
 git remote add origin https://github.com/AndroidIosWebEmbeddedFirmwareDver/YtDesignMasterServer.git
 #5.
